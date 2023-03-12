596ea68bcc0a99e39810bac3c40a586b39b304ca
Khushi Garg <khushigarg.64901@gmail.com>
2023-03-12 15:58:12 +0530

    second

--- src/Board.js
+++ src/Board.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import "./Board.css";
+
 class Board extends React.Component {
     constructor(props) {
         super(props);
-
         const { GridSize } = this.props;
 
-        // Initialize the game grid with the specified size
+        //-------Initialize the game grid with the specified size
         const grid = [];
         for (let i = 0; i < GridSize; i++) {
             grid.push([]);
             for (let j = 0; j < GridSize; j++) {
+                //-------Each cell is an object with a value and a merged flag
                 grid[i].push({ value: 0, merged: false });
             }
         }
-
-        // Add two random tiles to start the game
+        //-------Add two random tiles to start the game
         for (let i = 0; i < 2; i++) {
             this.addRandomTile(grid);
         }
-
         this.state = {
             grid,
             score: 0,
@@ -56,7 +55,7 @@ class Board extends React.Component {
     moveTilesLeft = () => {
         const { grid } = this.state;
 
-        // Move tiles to the left and merge them
+        //-------Move tiles to the left and merge them
         for (let i = 0; i < grid.length; i++) {
             let k = 0;
             for (let j = 0; j < grid.length; j++) {
@@ -66,7 +65,7 @@ class Board extends React.Component {
                         grid[i][j].value = 0;
                     }
                     if (k > 0 && grid[i][k - 1].value === grid[i][k].value && !grid[i][k - 1].merged) {
-                        // Merge tiles with the same value
+                        //-------Merge tiles with the same value
                         grid[i][k - 1].value *= 2;
                         grid[i][k - 1].merged = true;
                         grid[i][k].value = 0;
@@ -78,19 +77,17 @@ class Board extends React.Component {
             }
         }
 
-        // Add a new tile after moving
+        //-------Add a new tile after moving
         this.addRandomTile(grid);
-
-        // Reset the merged flag for all tiles
+        //-------Reset the merged flag for all tiles
         this.resetMergedFlag(grid);
-
         this.setState({ grid });
     };
 
     moveTilesRight = () => {
         const { grid } = this.state;
 
-        // Move tiles to the right and merge them
+        //-------Move tiles to the right and merge them
         for (let i = 0; i < grid.length; i++) {
             let k = grid.length - 1;
             for (let j = grid.length - 1; j >= 0; j--) {
@@ -100,7 +97,7 @@ class Board extends React.Component {
                         grid[i][j].value = 0;
                     }
                     if (k < grid.length - 1 && grid[i][k + 1].value === grid[i][k].value && !grid[i][k + 1].merged) {
-                        // Merge tiles with the same value
+                        //-------Merge tiles with the same value
                         grid[i][k + 1].value *= 2;
                         grid[i][k + 1].merged = true;
                         grid[i][k].value = 0;
@@ -111,17 +108,16 @@ class Board extends React.Component {
                 }
             }
         }
-        // Add a new tile after moving
+        //-------Add a new tile after moving
         this.addRandomTile(grid);
-
-        // Reset the merged flag for all tiles
+        //-------Reset the merged flag for all tiles
         this.resetMergedFlag(grid);
-
         this.setState({ grid });
     };
+
     moveTilesUp = () => {
         const { grid } = this.state;
-        // Move tiles up and merge them
+        //-------Move tiles up and merge them
         for (let j = 0; j < grid.length; j++) {
             let k = 0;
             for (let i = 0; i < grid.length; i++) {
@@ -131,7 +127,7 @@ class Board extends React.Component {
                         grid[i][j].value = 0;
                     }
                     if (k > 0 && grid[k - 1][j].value === grid[k][j].value && !grid[k - 1][j].merged) {
-                        // Merge tiles with the same value
+                        //-------Merge tiles with the same value
                         grid[k - 1][j].value *= 2;
                         grid[k - 1][j].merged = true;
                         grid[k][j].value = 0;
@@ -143,18 +139,16 @@ class Board extends React.Component {
             }
         }
 
-        // Add a new tile after moving
+        //-------Add a new tile after moving
         this.addRandomTile(grid);
-
-        // Reset the merged flag for all tiles
+        //-------Reset the merged flag for all tiles
         this.resetMergedFlag(grid);
-
         this.setState({ grid });
 
     };
     moveTilesDown = () => {
         const { grid } = this.state;
-        // Move tiles down and merge them
+        //-------Move tiles down and merge them
         for (let j = 0; j < grid.length; j++) {
             let k = grid.length - 1;
             for (let i = grid.length - 1; i >= 0; i--) {
@@ -164,7 +158,7 @@ class Board extends React.Component {
                         grid[i][j].value = 0;
                     }
                     if (k < grid.length - 1 && grid[k + 1][j].value === grid[k][j].value && !grid[k + 1][j].merged) {
-                        // Merge tiles with the same value
+                        //-------Merge tiles with the same value
                         grid[k + 1][j].value *= 2;
                         grid[k + 1][j].merged = true;
                         grid[k][j].value = 0;
@@ -176,17 +170,16 @@ class Board extends React.Component {
             }
         }
 
-        // Add a new tile after moving
+        //-------Add a new tile after moving
         this.addRandomTile(grid);
-
-        // Reset the merged flag for all tiles
+        //-------Reset the merged flag for all tiles
         this.resetMergedFlag(grid);
-
         this.setState({ grid });
     };
+
     addRandomTile = (grid) => {
         const { GridSize } = this.props;
-        // Find empty cells in the grid
+        //-------Find empty cells in the grid
         const emptyCells = [];
         for (let i = 0; i < GridSize; i++) {
             for (let j = 0; j < GridSize; j++) {
@@ -195,7 +188,7 @@ class Board extends React.Component {
                 }
             }
 
-            // Choose a random empty cell and add a new tile with a value of 2 or 4
+            //-------Choose a random empty cell and add a new tile with a value of 2 or 4
             if (emptyCells.length > 0) {
                 const { x, y } = emptyCells[Math.floor(Math.random() * emptyCells.length)];
                 grid[x][y].value = Math.random() < 0.9 ? 2 : 4;
@@ -204,7 +197,7 @@ class Board extends React.Component {
         };
     }
     resetMergedFlag = (grid) => {
-        // Reset the merged flag for all tiles
+        //-------Reset the merged flag for all tiles
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid.length; j++) {
                 grid[i][j].merged = false;
